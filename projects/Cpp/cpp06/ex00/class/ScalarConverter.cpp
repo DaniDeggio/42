afc2448820de0bcb004cd76b0f7593c949bd118a
DanyDeggio <danideg12@gmail.com>
2025-01-23 17:39:28 +0100

    Cpp06 ex00

--- projects/Cpp/cpp06/ex00/class/ScalarConverter.cpp
+++ projects/Cpp/cpp06/ex00/class/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 19:12:41 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/01/21 05:59:39 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/01/23 17:38:40 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,29 +28,45 @@ ScalarConverter::~ScalarConverter(void) {
 }
 
 void ScalarConverter::convert(std::string literal) {
-	try {
-		char c = static_cast<char>(std::stoi(literal));
-		std::cout << "char: '" << c << "'" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << "char: impossible" << std::endl;
+	std::string pseudo[8] = {"inff", "+inff", "-inff", "nanf", "inf", "+inf", "-inf", "nan"};
+
+	for (int i = 0; i < 8; i++) {
+		if (literal == pseudo[i]) {
+			float f = std::atof(literal.c_str());
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << f << "f" << std::endl;
+			std::cout << "double: " << static_cast<double>(f) << std::endl;
+			return;
+		}
 	}
-	try {
-		int i = static_cast<int>(std::stoi(literal));
-		std::cout << "int: " << i << std::endl;
-	} catch
-	(std::exception &e) {
-		std::cout << "int: impossible" << std::endl;
-	}
-	try {
-		float f = static_cast<float>(std::stof(literal));
-		std::cout << "float: " << f << "f" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << "float: impossible" << std::endl;
-	}
-	try {
-		double d = static_cast<double>(std::stod(literal));
-		std::cout << "double: " << d << std::endl;
-	} catch (std::exception &e) {
-		std::cout << "double: impossible" << std::endl;
-	}
+// 	try {
+// 		int i = std::atoi(literal.c_str());
+// 		if (i < 32 || i > 126 || i == 127)
+// 			throw std::exception();
+// 		char c = static_cast<char>(std::atoi(literal.c_str()));
+// 		std::cout << "char: '" << c << "'" << std::endl;
+// 	} catch (std::exception &e) {
+// 		std::cout << "char: impossible" << std::endl;
+// 	}
+	
+// 	try {
+// 		int i = static_cast<int>(std::atoi(literal.c_str()));
+// 		std::cout << "int: " << i << std::endl;
+// 	} catch
+// 	(std::exception &e) {
+// 		std::cout << "int: impossible" << std::endl;
+// 	}
+// 	try {
+// 		float f = static_cast<float>(std::atof(literal.c_str()));
+// 		std::cout << "float: " << f << "f" << std::endl;
+// 	} catch (std::exception &e) {
+// 		std::cout << "float: impossible" << std::endl;
+// 	}
+// 	try {
+// 		double d = static_cast<double>(std::atof(literal.c_str()));
+// 		std::cout << "double: " << d << std::endl;
+// 	} catch (std::exception &e) {
+// 		std::cout << "double: impossible" << std::endl;
+// 	}
 }
--- projects/Cpp/cpp06/ex00/srcs/main.cpp
+++ projects/Cpp/cpp06/ex00/srcs/main.cpp
@@ -6,85 +6,18 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:54:02 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/01/17 04:18:46 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/01/21 20:23:08 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../class/Bureaucrat.hpp"
-#include "../class/AForm.hpp"
-#include "../class/ShrubberyCreationForm.hpp"
-#include "../class/RobotomyRequestForm.hpp"
-#include "../class/PresidentialPardonForm.hpp"
-#include "../class/Intern.hpp"
+#include "../class/ScalarConverter.hpp"
 
-int main(){
-	try
-	{
-		Bureaucrat b1("Bureaucrat1", 120);
-		Bureaucrat b2("Bureaucrat2", 140);
-		
-		AForm *f1 = new ShrubberyCreationForm("TEST");
-		b2.signForm(*f1);
-		b2.executeForm(*f1);
-		b1.executeForm(*f1);
-		delete f1;
-	} catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cout << "Usage: ./convert [literal]" << std::endl;
+		return 1;
 	}
-	
-	std::cout << std::endl;
-
-	try
-	{
-		Bureaucrat b1("Bureaucrat1", 40);
-		Bureaucrat b2("Bureaucrat2", 60);
-		
-		AForm *f1 = new RobotomyRequestForm("TEST");
-		b2.signForm(*f1);
-		b2.executeForm(*f1);
-		b1.executeForm(*f1);
-		delete f1;
-	} catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	
-	std::cout << std::endl;
-
-	try
-	{
-		Bureaucrat b1("Bureaucrat1", 10);
-		Bureaucrat b2("Bureaucrat2", 2);
-		
-		AForm *f1 = new PresidentialPardonForm("TEST");
-		b2.signForm(*f1);
-		b2.executeForm(*f1);
-		b1.executeForm(*f1);
-		delete f1;
-	} catch (std::exception & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	
-	std::cout << std::endl;
-
-    try {
-        Intern someRandomIntern;
-        AForm* rrf;
-        
-
-        rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-        if (rrf) {
-            Bureaucrat b1("Bureaucrat1", 40);
-            b1.signForm(*rrf);
-            b1.executeForm(*rrf);
-            delete rrf;
-        }
-        
-    } catch (std::exception & e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << std::endl;
+	ScalarConverter sc;
+	sc.convert(av[1]);
+	return 0;
 }
