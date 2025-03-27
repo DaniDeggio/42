7988354040a97eca13ff207e6dc54062423ec1e0
DanyDeggio <danideg12@gmail.com>
2025-03-27 22:49:44 +0100

    fix: removed forgotten usless function ex01

--- projects/Cpp/cpp07/ex01/include/iter.hpp
+++ projects/Cpp/cpp07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 22:37:49 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/03/27 22:42:42 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/03/27 22:49:09 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 # define ITER_HPP
 
 #include <iostream>
-
-template <typename T>
-void swap(T &a, T &b) {
-	T tmp = a;
-	a = b;
-	b = tmp;
-}
 
 template <typename T, typename U, typename V>
 void iter(T *array, U length, V func) {
