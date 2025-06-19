e4efb2817c44d7cf0de5ed218776f8126b2b2c2f
Danilo De Giovanni <dde-giov@e3r2p3.42roma.it>
2025-06-19 18:11:12 +0200

    cpp08 ex02 init

---
+++ projects/Cpp/cpp08/ex02/class/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 22:37:49 by dde-giov          #+#    #+#             */
+/*   Updated: 2025/06/19 18:09:40 by dde-giov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>{
+	private:
+
+	public:
+
+};
+
+#endif
--- projects/Cpp/cpp08/ex02/srcs/main.cpp
+++ projects/Cpp/cpp08/ex02/srcs/main.cpp
@@ -6,98 +6,36 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 23:38:13 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/04/01 07:14:12 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/06/19 18:03:01 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
-#include "../include/Array.hpp"
+#include "../class/MutantStack.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int main()
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-		std::cout << "trying negative index" << std::endl;
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-		std::cout << "trying out of range index" << std::endl;
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-
-	
-	// Test: deep copy
-	std::cout << std::endl << "Testing deep copy" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
 	{
-		Array<int> tmp = numbers;
-		Array<int> test(tmp);
-
-		numbers[0] = 999999;
-		if (tmp[0] == numbers[0])
-			std::cerr << "Deep copy failed!" << std::endl;
-		tmp[0] = numbers[0];
-		if (test[0] == numbers[0])
-			std::cerr << "Deep copy failed!" << std::endl;
+		std::cout << *it << std::endl;
+		++it;
 	}
-
-	// Test: Array string
-	std::cout << std::endl << "Testing Array<std::string>" << std::endl;
-	Array<std::string> strArr(3);
-	strArr[0] = "42";
-	strArr[1] = "Roma";
-	strArr[2] = "dde-giov";
-	for (unsigned int i = 0; i < strArr.size(); ++i)
-		std::cout << "strArr[" << i << "] = " << strArr[i] << std::endl;
-
-	// Test: empty array
-	std::cout << std::endl << "Testing empty array" << std::endl;
-	Array<float> emptyArr;
-	std::cout << "emptyArr.size() = " << emptyArr.size() << std::endl;
-	try {
-		emptyArr[0] = 3.14f;
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-	}
-
+	std::stack<int> s(mstack);
 	return 0;
 }
