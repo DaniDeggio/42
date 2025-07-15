f47a48cf1be4360e9fbc4b699c0bb2137f3a4407
DanyDeggio <danideg12@gmail.com>
2025-07-15 17:13:39 +0200

    feat: cpp08 finished

--- projects/Cpp/cpp08/ex02/class/MutantStack.hpp
+++ projects/Cpp/cpp08/ex02/class/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 22:37:49 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/06/19 18:09:40 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/07/15 17:13:04 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,38 @@ class MutantStack : public std::stack<T>{
 	private:
 
 	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end() {
+			return this->c.end();
+		}
+
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+		const_iterator end() const {
+			return this->c.end();
+		}
+
+		reverse_iterator rbegin() {
+			return this->c.rbegin();
+		}
+		reverse_iterator rend() {
+			return this->c.rend();
+		}
+
+		const_reverse_iterator rbegin() const {
+			return this->c.rbegin();
+		}
+		const_reverse_iterator rend() const {
+			return this->c.rend();
+		}
 };
 
 #endif
--- projects/Cpp/cpp08/ex02/srcs/main.cpp
+++ projects/Cpp/cpp08/ex02/srcs/main.cpp
@@ -6,36 +6,64 @@
 /*   By: dde-giov <dde-giov@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 23:38:13 by dde-giov          #+#    #+#             */
-/*   Updated: 2025/06/19 18:03:01 by dde-giov         ###   ########.fr       */
+/*   Updated: 2025/07/15 17:11:25 by dde-giov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
+#include <list>
+
 #include "../class/MutantStack.hpp"
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	{
+		std::cout << std::endl << "Testing with list:" << std::endl;
+		
+		std::list<int> list;
+		list.push_back(5);
+		list.push_back(17);
+		std::cout << list.back() << std::endl;
+		list.pop_back();
+		std::cout << list.size() << std::endl;
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		//[...]
+		list.push_back(0);
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return 0;
 }
